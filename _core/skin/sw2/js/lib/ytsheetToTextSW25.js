9d1dcaa962f12bb2b9d0c7d9c3b2fcaf2e2e2a21
Shunshun94 <shunshun9460@gmail.com>
2020-10-27 01:59:10 +0900

    catch up lib info

--- _core/skin/sw2/js/lib/ytsheetToTextSW25.js
+++ _core/skin/sw2/js/lib/ytsheetToTextSW25.js
@@ -1,23 +1,79 @@
-/*  Copyright 2020 @Shunshun94
+/* MIT License
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+Copyright 2020 @Shunshun94
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 var io = io || {};
 io.github = io.github || {};
 io.github.shunshun94 = io.github.shunshun94 || {};
 io.github.shunshun94.trpg = io.github.shunshun94.trpg || {};
 io.github.shunshun94.trpg.ytsheet = io.github.shunshun94.trpg.ytsheet || {};
+
+io.github.shunshun94.trpg.ytsheet.length = (str='') => {
+	// https://zukucode.com/2017/04/javascript-string-length.html
+	let length = 0;
+	for (let i = 0; i < str.length; i++) {
+			const c = str.charCodeAt(i);
+			if ((c >= 0x0 && c < 0x81) || (c === 0xf8f0) || (c >= 0xff61 && c < 0xffa0) || (c >= 0xf8f1 && c < 0xf8f4)) {
+				length += 1;
+			} else {
+				length += 2;
+			}
+	}
+	return length;
+};
+
+io.github.shunshun94.trpg.ytsheet._getColumnLength = (list, header) => {
+	return list.reduce((currentMax, targetEffect)=>{
+		const result = {};
+		for(var key in currentMax) {
+			result[key] = Math.max(io.github.shunshun94.trpg.ytsheet.length(targetEffect[key]), currentMax[key]);
+		}
+		return result;
+	}, header);
+};
+
+io.github.shunshun94.trpg.ytsheet._convertList = (list, columns) => {
+	const headerLength = io.github.shunshun94.trpg.ytsheet._getLengthWithoutNote(columns);
+	const length = io.github.shunshun94.trpg.ytsheet._getColumnLength(list, headerLength);
+	const convertDataToString = (data) => {
+		const result = [];
+		for(var key in headerLength) {
+			result.push(`${data[key]}${''.padEnd(length[key] - io.github.shunshun94.trpg.ytsheet.length(data[key]), ' ')}`);
+		}
+		result.push(data.note);
+		return result.join('/');
+	};
+	return [columns].concat(list).map(convertDataToString).join('\n');
+};
+
+io.github.shunshun94.trpg.ytsheet._getLengthWithoutNote = (baseHeader) => {
+	const result = {};
+	for(let key in baseHeader) {
+		if(key !== 'note') {
+			result[key] = io.github.shunshun94.trpg.ytsheet.length(baseHeader[key]);
+		}
+	}
+	return result;
+};
+
 io.github.shunshun94.trpg.ytsheet.generateCharacterTextFromYtSheet2SwordWorldEnemy = (json) => {
 	const result = [];
 
