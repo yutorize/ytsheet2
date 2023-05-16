49c8090868d231935449e61d66261cf7ef4c3f18
ViVi <vivi.shark.tech+github@gmail.com>
2023-05-16 23:02:56 +0900

    [DX3] 「ブリード」に有効な値がないとき、表示を空にする

--- _core/lib/dx3/view-chara.pl
+++ _core/lib/dx3/view-chara.pl
@@ -256,8 +256,9 @@ $SHEET->param(wordsY => ($pc{'wordsY'} eq '下' ? 'bottom:0;' : 'top:0;'));
 if($pc{'stage'} =~ /クロウリングケイオス/){ $SHEET->param(ccOn => 1); }
 
 ### ブリード --------------------------------------------------
-$SHEET->param(breed => 
-  ($pc{'breed'} ? $pc{'breed'} : $pc{'syndrome3'} ? 'トライ' : $pc{'syndrome2'} ? 'クロス' : $pc{'syndrome1'} ? 'ピュア' : '') . '<span>ブリード</span>'
+my $breedPrefix = ($pc{'breed'} ? $pc{'breed'} : $pc{'syndrome3'} ? 'トライ' : $pc{'syndrome2'} ? 'クロス' : $pc{'syndrome1'} ? 'ピュア' : '');
+$SHEET->param(breed =>
+    $breedPrefix ? $breedPrefix . '<span>ブリード</span>' : ''
 );
 
 ### 能力値 --------------------------------------------------
