6de59c2f41cf74fa12ee5f20b2d0ba7c10752446
ViVi <vivi.shark.tech+github@gmail.com>
2023-05-19 15:41:02 +0900

    [VC] セッション履歴の空の行は表示しない

--- _core/lib/vc/view-chara.pl
+++ _core/lib/vc/view-chara.pl
@@ -281,6 +281,9 @@ foreach (0 .. $pc{'historyNum'}){
   }
   $pc{'history'.$_.'Money'} =~ s/([0-9]+)/$1<wbr>/g;
   $pc{'history'.$_.'Money'} =~ s/([0-9]+)/commify($1);/ge;
+  if (!($pc{'history'.$_.'Gm'} || $pc{'history'.$_.'Date'} || $pc{'history'.$_.'Title'} || $pc{'history'.$_.'Result'} || $members || $pc{'history'.$_.'Note'})) {
+    next;
+  }
   push(@history, {
     "NUM"    => ($pc{'history'.$_.'Gm'} ? $h_num : ''),
     "DATE"   => $pc{'history'.$_.'Date'},
