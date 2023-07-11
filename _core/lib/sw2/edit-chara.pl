cc624f4c86c47beb4e5cf37379f5b8d6befcdb57
TALE <42292222+yutorize@users.noreply.github.com>
2023-07-11 13:22:58 +0900

    :bug: 修正: [SW] 能力作成板からの新規作成で、希少種のある種族は希少種が初期選択になってしまう

--- _core/lib/sw2/edit-chara.pl
+++ _core/lib/sw2/edit-chara.pl
@@ -61,7 +61,7 @@ elsif($mode eq 'blanksheet'){
   if($::in{'stt'}){
     ($pc{'sttBaseTec'}, $pc{'sttBasePhy'}, $pc{'sttBaseSpi'}, $pc{'sttBaseA'}, $pc{'sttBaseB'}, $pc{'sttBaseC'}, $pc{'sttBaseD'}, $pc{'sttBaseE'}, $pc{'sttBaseF'}) = split(/_/, $::in{'stt'});
     $pc{'race'} = Encode::decode('utf8', $::in{'race'});
-    if($data::races{$pc{'race'}}{'variant'}){
+    if($data::races{$pc{'race'}}{'variant'} && !$data::races{$pc{'race'}}{'ability'}){
       $pc{'race'} .= "（$data::races{$pc{'race'}}{'variantSort'}[0]）";
     }
     if($::in{'making_num'}){
