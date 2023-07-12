acb2a631f956968db2d94c12242b45a2f5d6f570
RYOSKATE <ryoskate@gmail.com>
2023-07-12 12:55:01 +0900

    ロイスの「関係」に入力候補を設定

--- _core/lib/dx3/edit-chara.pl
+++ _core/lib/dx3/edit-chara.pl
@@ -557,7 +557,7 @@ foreach my $num (1 .. 7) {
 if(!$pc{"lois${num}State"}){ $pc{"lois${num}State"} = 'ロイス' }
 print <<"HTML";
             <tr id="lois${num}">
-              <td><span class="handle"></span>@{[input "lois${num}Relation"]}
+              <td><span class="handle"></span>@{[input "lois${num}Relation", '', '', 'list="list-lois-relation"']}
               <td>@{[input "lois${num}Name",'','encroachBonusType']}
               <td class="emo">@{[input "lois${num}EmoPosiCheck",'checkbox',"emoP($num)"]}@{[input "lois${num}EmoPosi",'','','list="list-emotionP"']}
               <td>／
@@ -1152,6 +1152,9 @@ print <<"HTML";
   </datalist>
   <datalist id="list-blood">
     <option value="A型"><option value="B型"><option value="AB型"><option value="O型"><option value="不明"><option value="不詳">
+  </datalist>
+  <datalist id="list-lois-relation">
+    <option value="Dロイス">
   </datalist>
   <datalist id="list-emotionP">
     <option value="傾倒">
