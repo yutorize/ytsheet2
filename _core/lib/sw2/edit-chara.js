4b57b454f76d5c14a94ac5e4514422d18bdb1148
TALE <42292222+yutorize@users.noreply.github.com>
2024-05-24 15:35:35 +0900

    :bug: 修正: [SW2] 言語欄の通知メッセージの順番が一定しない

--- _core/lib/sw2/edit-chara.js
+++ _core/lib/sw2/edit-chara.js
@@ -1687,7 +1687,7 @@ function checkLanguage(){
     count[read.value] ||= 0; count[read.value]++;
   }
   let notice = '';
-  for (let key in SET.class){
+  for (let key of SET.classNames){
     if(!SET.class[key].language){ continue; }
     const className = key;
     const classId = SET.class[key].id;
