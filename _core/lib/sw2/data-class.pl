279cf440defc22552b5b1da6fed83e91bdc13206
Jun Kudou <prog@akanekodou.net>
2024-06-27 19:15:11 +0900

    [SW2] ミスティック観察パッケージを追加
    探索判定、天候予測判定

--- _core/lib/sw2/data-class.pl
+++ _core/lib/sw2/data-class.pl
@@ -582,6 +582,9 @@ our %class = (
     expTable => 'B',
     id       => 'Mys',
     eName    => 'mystic',
+    package  => {
+      Obs => { name => '観察', stt => 'E' },
+    },
     craft => {
       jName => '占瞳',
       eName => 'divination',
