761be82c4077c840915ebb257fbdd94afd7227c5
ViVi <vivi.shark.tech+github@gmail.com>
2024-09-30 15:00:32 +0900

    [SW2] 複数部位の魔物データを Udonarium 向けに出力する際の、防護点を記述する項目の名称を変更

--- _core/lib/sw2/subroutine-sw2.pl
+++ _core/lib/sw2/subroutine-sw2.pl
@@ -11,6 +11,7 @@ use Fcntl;
 ### ユニットステータス出力 --------------------------------------------------
 sub createUnitStatus {
   my %pc = %{$_[0]};
+  my $target = $_[1] || '';
   my @unitStatus;
   if ($pc{type} eq 'm'){
     my @n2a = ('','A' .. 'Z');
@@ -44,7 +45,11 @@ sub createUnitStatus {
       @unitStatus = ();
       push(@unitStatus, @hp);
       push(@unitStatus, @mp) if $#mp >= 0;
-      push(@unitStatus, {'メモ' => '防護:'.join('／',@def)});
+      if ($target eq 'udonarium') {
+        push(@unitStatus, {'防護' => join('／',@def)});
+      } else {
+        push(@unitStatus, {'メモ' => '防護:'.join('／',@def)});
+      }
     }
     else { # 1部位
       my $i = 1;
