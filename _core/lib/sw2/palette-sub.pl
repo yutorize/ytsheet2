76fc4cb4cd29ead8b9f2b8c711a6f2ce9cebe315
ViVi <vivi.shark.tech+github@gmail.com>
2024-10-23 22:57:34 +0900

    [SW2] Fix: 【カオスエクスプロージョン】用のチャットパレットの生成条件を修正

--- _core/lib/sw2/palette-sub.pl
+++ _core/lib/sw2/palette-sub.pl
@@ -244,6 +244,7 @@ sub palettePreset {
           next if($pows{$id}{$pow} > $::pc{'lv'.$id} && $id ne 'Fai');
           next if($id eq 'Wiz' && $pows{$id}{$pow} > min($::pc{lvSor},$::pc{lvCon}));
           next if($id eq 'Fai' && $pows{$id}{$pow} > fairyRank($::pc{lvFai},$::pc{fairyContractEarth},$::pc{fairyContractWater},$::pc{fairyContractFire },$::pc{fairyContractWind },$::pc{fairyContractLight},$::pc{fairyContractDark }));
+          next if $id eq 'Fai' && $pow == 80 && $::pc{lvFai} < 15;
           if($id eq 'Bar'){ $pow += $::pc{finaleEnhance} || 0; }
 
           $text .= "k${pow}[{魔法C}$activeCrit]+$magicPower".addNum($::pc{'magicDamageAdd'.$id})."+{魔法D修正}$activeDmg ダメージ\n";
