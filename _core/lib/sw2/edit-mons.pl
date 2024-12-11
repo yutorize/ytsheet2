d5ae6496b9dd66eb4567a09dda53e147222a5bb1
ViVi <44130782+ViVi-shark@users.noreply.github.com>
2024-12-11 17:46:36 +0900

    :zap: 追加: [SW2] 魔物データのコア部位入力欄において、部位内訳にもとづく入力候補を提供 (#274)
    Co-authored-by: ViVi <vivi.shark.tech+github@gmail.com>

--- _core/lib/sw2/edit-mons.pl
+++ _core/lib/sw2/edit-mons.pl
@@ -353,8 +353,9 @@ print <<"HTML";
       </div>
       <div class="box parts in-toc" data-content-title="部位数・コア部位">
         @{[ checkbox 'partsManualInput', '部位数と内訳を手動入力する', 'updatePartsAutomatically' ]}
-        <dl><dt>部位数<dd>@{[ input 'partsNum','number','','min="1"' ]} (@{[ input 'parts' ]}) </dl>
-        <dl><dt>コア部位<dd>@{[ input 'coreParts' ]}</dl>
+        <dl><dt>部位数<dd>@{[ input 'partsNum','number','','min="1"' ]} (@{[ input 'parts','','updatePartList' ]}) </dl>
+        <dl><dt>コア部位<dd>@{[ input 'coreParts','','','list="list-of-core-part"' ]}</dl>
+        <datalist id="list-of-core-part"></datalist>
       </div>
       <div class="box">
         <h2 class="in-toc">特殊能力</h2>
