0d8eb3d195a3a6e3df6cf89b1f388117554e14d4
ViVi <vivi.shark.tech+github@gmail.com>
2024-12-27 10:29:20 +0900

    [SW2.5] 閲覧画面において、不名誉点の残高をカンマ区切りにする

--- _core/lib/sw2/view-chara.pl
+++ _core/lib/sw2/view-chara.pl
@@ -1042,6 +1042,7 @@ $SHEET->param(historyMoneyTotal => commify $pc{historyMoneyTotal} );
 $SHEET->param(honor => commify($pc{honor}));
 $SHEET->param(honorOffset => commify($pc{honorOffset})) if $pc{honorOffset};
 $SHEET->param(honorOffsetBarbaros => commify($pc{honorOffsetBarbaros})) if $pc{honorOffsetBarbaros};
+$SHEET->param(dishonor => commify($pc{dishonor})) if $pc{dishonor};
 
 my @honoritems;
 foreach (1 .. $pc{honorItemsNum}) {
