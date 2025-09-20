2b1c065613177f953e9c08615a3dbc75f171320c
ViVi <vivi.shark.tech+github@gmail.com>
2025-09-20 10:28:13 +0900

    [SW2.5] ユニットのステータス項目に妖精魔法の契約属性を追加

--- _core/lib/sw2/subroutine-sw2.pl
+++ _core/lib/sw2/subroutine-sw2.pl
@@ -75,6 +75,15 @@ sub createUnitStatus {
     );
 
     if (!$::SW2_0) {
+      if ($pc{lvFai}) {
+        my @contractAttributes = ();
+        foreach ([Earth => '土'], [Water => '水'], [Fire => '炎'], [Wind => '風'], [Light => '光'], [Dark => '闇']) {
+          (my $attributeEn, my $attributeJa) = @{$_};
+          next unless $pc{"fairyContract${attributeEn}"};
+          push(@contractAttributes, $attributeJa);
+        }
+        push(@unitStatus, { 契約属性 => join('', @contractAttributes) });
+      }
       if ($pc{lvBar}) {
         push(@unitStatus, { '⤴' => '0' });
         push(@unitStatus, { '⤵' => '0' });
