e9fda7b61995b71e32b72e8941c7ec6acb33a273
ViVi <44130782+ViVi-shark@users.noreply.github.com>
2026-05-02 16:31:42 +0900

    :zap: 追加: [SW2.5] 楽素記号をあらわす記法 (#375)
    Co-Authored-By: ViVi <vivi.shark.tech+github@gmail.com>
    Co-Authored-By: TALE <42292222+yutorize@users.noreply.github.com>

--- _core/lib/subroutine.pl
+++ _core/lib/subroutine.pl
@@ -492,6 +492,10 @@ sub unescapeTags {
       $text =~ s/(\[[常準主補宣]\])+/&textToIcon($&);/egi;
       $text =~ s/「((?:[○◯〇△＞▶〆☆≫»□☐☑🗨]|&gt;&gt;)+)/"「".&textToIcon($1);/egi;
     }
+
+    $text =~ s|\[[⤴↑]\]|<i class="s-icon uplift">⤴</i>|g;
+    $text =~ s|\[[⤵↓]\]|<i class="s-icon calm">⤵</i>|g;
+    $text =~ s|\[♡\]|<i class="s-icon heart">♡</i>|g;
   }
   
   
@@ -542,12 +546,12 @@ sub unescapeTagsLines {
   $text =~ s/^RIGHT:/<\/p><p class="right">/gim;
   
   my $d_count = 0;
-  $d_count += ($text =~ s/^\[(&gt;|[↓vＶｖ])\]\*\*\*\*(.*?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary class=\"header4\">$2<\/summary><div class=\"detail-body\"><p>"/gime);
-  $d_count += ($text =~ s/^\[(&gt;|[↓vＶｖ])\]\*\*\*(.*?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary class=\"header3\">$2<\/summary><div class=\"detail-body\"><p>"/gime);
-  $d_count += ($text =~ s/^\[(&gt;|[↓vＶｖ])\]\*\*(.*?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary class=\"header2\">$2<\/summary><div class=\"detail-body\"><p>"/gime);
-  $d_count += ($text =~ s/^\[(&gt;|[↓vＶｖ])\]\*(.*?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary class=\"header1\">$2<\/summary><div class=\"detail-bod\"><p>"/gime);
-  $d_count += ($text =~ s/^\[(&gt;|[↓vＶｖ])\](.+?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary>$2<\/summary><div class=\"detail-body\"><p>"/gime);
-  $d_count += ($text =~ s/^\[(&gt;|[↓vＶｖ])\]$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary>詳細<\/summary><div class=\"detail-body\"><p>"/gime);
+  $d_count += ($text =~ s/^\[(&gt;|[vＶｖ])\]\*\*\*\*(.*?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary class=\"header4\">$2<\/summary><div class=\"detail-body\"><p>"/gime);
+  $d_count += ($text =~ s/^\[(&gt;|[vＶｖ])\]\*\*\*(.*?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary class=\"header3\">$2<\/summary><div class=\"detail-body\"><p>"/gime);
+  $d_count += ($text =~ s/^\[(&gt;|[vＶｖ])\]\*\*(.*?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary class=\"header2\">$2<\/summary><div class=\"detail-body\"><p>"/gime);
+  $d_count += ($text =~ s/^\[(&gt;|[vＶｖ])\]\*(.*?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary class=\"header1\">$2<\/summary><div class=\"detail-bod\"><p>"/gime);
+  $d_count += ($text =~ s/^\[(&gt;|[vＶｖ])\](.+?)$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary>$2<\/summary><div class=\"detail-body\"><p>"/gime);
+  $d_count += ($text =~ s/^\[(&gt;|[vＶｖ])\]$/"<\/p><details @{[$1 eq '&gt;' ? '' : 'open']}><summary>詳細<\/summary><div class=\"detail-body\"><p>"/gime);
   $d_count -= ($text =~ s/^\[-{3,}\]\n?$/<\/p><\/div><\/details><p>/gim);
   
   $text =~ s/^-{4,}$/<\/p><hr><p>/gim;  
