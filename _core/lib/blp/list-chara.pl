de294074c5dbdc67ac50a7c49493433d1300710f
TALE <42292222+yutorize@users.noreply.github.com>
2026-05-06 12:06:38 +0900

    :zap: 改善: 一覧の表示速度向上

--- _core/lib/blp/list-chara.pl
+++ _core/lib/blp/list-chara.pl
@@ -57,42 +57,32 @@ foreach(
 my $q_links = @q_links ? '&'.join('&', @q_links) : '';
 
 ### ファイル読み込み --------------------------------------------------
-## マイリスト取得
-my @mylist;
-if($mode eq 'mylist'){
-  $INDEX->param( playerName => (getplayername($LOGIN_ID))[0] );
-  open (my $FH, "<", $set::passfile);
-  while(my $line = <$FH>){
-    if($line =~ /^(.+?)<>\[$LOGIN_ID\]</){ push(@mylist, $1) }
-  }
-  close($FH);
-}
-
-## リスト取得
 my @list;
-if($set::simpleindex && $index_mode) { #グループ見出しのみ
+#グループ見出しのみ
+if($set::simpleindex && $index_mode) {
   $INDEX->param(simpleIndex => 1);
 }
-else { #通常
-  open (my $FH, "<", $set::listfile);
-  @list = <$FH>;
-  close($FH);
+#通常
+else {
+  # マイリスト
+  if($mode eq 'mylist'){
+    $INDEX->param( playerName => (getplayername($LOGIN_ID))[0] );
+    @list = getMylist($LOGIN_ID);
+  }
+  else {
+    open (my $FH, "<", $set::listfile);
+    # 管理者orタグ検索（全読込）
+    if(($set::masterid && $set::masterid eq $LOGIN_ID) || $::in{tag}){
+      @list = <$FH>;
+    }
+    # 非表示除外
+    else {
+      @list = grep { !/^(?:[^<]*<>){19}[^<0]/ } <$FH>;
+    }
+    close($FH);
+  }
 }
-### フィルタ処理 --------------------------------------------------
-## マイリスト
-if($mode eq 'mylist'){
-  my $regex = join('|', @mylist);
-  @list = grep { $_ =~ /^(?:$regex)\</ } @list;
-}
-## 非表示除外
-elsif (
-     !($set::masterid && $set::masterid eq $LOGIN_ID)
-  && !($mode eq 'mylist')
-  && !$::in{tag}
-){
-  @list = grep { !(split(/<>/))[19] } @list;
-}
-
+### 検索処理 --------------------------------------------------
 ## グループ検索
 my $group_query = $::in{group};
 my %groups = groupArrayToHash();
@@ -100,66 +90,74 @@ $groups{all}{name} = 'すべて' if $::in{group} eq 'all';
 $INDEX->param(Groups => groupArrayToList $group_query);
 
 if($group_query && $::in{group} ne 'all') {
-  if($group_query eq $set::group_default){ @list = grep { $_ =~ /^(?:[^<]*?<>){6}(\Q$group_query\E)?</ } @list; }
-  else { @list = grep { $_ =~ /^(?:[^<]*?<>){6}\Q$group_query\E</ } @list; }
+  if($group_query eq $set::group_default){ @list = grep { /^(?:[^<]*<>){6}(\Q$group_query\E)?</ } @list; }
+  else { @list = grep { /^(?:[^<]*<>){6}\Q$group_query\E</ } @list; }
 }
 $INDEX->param(group => $groups{$group_query}{name});
 
 ## タグ検索
 my $tag_query = normalizeHashtags(decode('utf8', $::in{tag}));
-if($tag_query) { @list = grep { $_ =~ /^(?:[^<]*?<>){18}[^<]*? \Q$tag_query\E / } @list; }
+if($tag_query) { @list = grep { /^(?:[^<]*<>){18}[^<]*? \Q$tag_query\E / } @list; }
 $INDEX->param(tag => $tag_query);
 
 ## 名前検索
 my $name_query = decode('utf8', $::in{name});
-if($name_query) { @list = grep { $_ =~ /^(?:[^<]*?<>){4}[^<]*?\Q$name_query\E/i } @list; }
+if($name_query) { @list = grep { /^(?:[^<]*<>){4}[^<]*?\Q$name_query\E/i } @list; }
 $INDEX->param(name => $name_query);
 
 ## PL名検索
 my $pl_query = decode('utf8', $::in{player});
-if($pl_query) { @list = grep { $_ =~ /^(?:[^<]*?<>){5}[^<]*?\Q$pl_query\E/i } @list; }
+if($pl_query) { @list = grep { /^(?:[^<]*<>){5}[^<]*?\Q$pl_query\E/i } @list; }
 $INDEX->param(player => $pl_query);
 
 ## ファクター検索
 my @factor_query = split('\s', decode('utf8', $::in{factor}));
-foreach my $q (@factor_query) { @list = grep { $_ =~ /^(?:[^<]*?<>){7,8}[^<]*?\Q$q\E/ } @list; }
+foreach my $q (@factor_query) { @list = grep { /^(?:[^<]*<>){7,8}[^<]*?\Q$q\E/ } @list; }
 $INDEX->param(factor => "@factor_query");
 
 ## 所属検索
 my @belong_query = split('\s', decode('utf8', $::in{belong}));
-foreach my $q (@belong_query) { @list = grep { $_ =~ /^(?:[^<]*?<>){13}[^<]*?\Q$q\E/ } @list; }
+foreach my $q (@belong_query) { @list = grep { /^(?:[^<]*<>){13}[^<]*?\Q$q\E/ } @list; }
 $INDEX->param(belong => "@belong_query");
 
 ## 喪失検索
 my @missing_query = split('\s', decode('utf8', $::in{missing}));
-foreach my $q (@missing_query) { @list = grep { $_ =~ /^(?:[^<]*?<>){14}[^<]*?\Q$q\E/ } @list; }
+foreach my $q (@missing_query) { @list = grep { /^(?:[^<]*<>){14}[^<]*?\Q$q\E/ } @list; }
 $INDEX->param(missing => "@missing_query");
 
 ## 画像フィルタ
 if($::in{image} == 1) {
-  @list = grep { $_ =~ /^(?:[^<]*?<>){17}[^<0]/ } @list;
+  @list = grep { /^(?:[^<]*<>){17}[^<0]/ } @list;
   $INDEX->param(image => 1);
 }
 elsif($::in{image} eq 'N') {
-  @list = grep { $_ !~ /^(?:[^<]*?<>){17}[^<0]/ } @list;
+  @list = grep { !/^(?:[^<]*<>){17}[^<0]/ } @list;
   $INDEX->param(image => 1);
 }
 
 ### ソート --------------------------------------------------
-if   ($sort eq 'name')  { my @tmp = map { sortName((split /<>/)[4]) } @list; @list = @list[sort {$tmp[$a] cmp $tmp[$b]} 0 .. $#tmp]; }
-elsif($sort eq 'pl')    { my @tmp = map { (split /<>/)[5]           } @list; @list = @list[sort {$tmp[$a] cmp $tmp[$b]} 0 .. $#tmp]; }
-elsif($sort eq 'date')  { my @tmp = map { (split /<>/)[3]           } @list; @list = @list[sort {$tmp[$b] <=> $tmp[$a]} 0 .. $#tmp]; }
-elsif($sort eq 'level') { my @tmp = map { (split /<>/)[15]          } @list; @list = @list[sort {$tmp[$b] <=> $tmp[$a]} 0 .. $#tmp]; }
-elsif($sort eq 'age')   { my @tmp = map { (split /<>/)[11]          } @list; @list = @list[sort {$tmp[$a] cmp $tmp[$b]} 0 .. $#tmp]; }
+if   ($sort eq 'name') { my @t = map { sortName($_)                   } @list; @list = @list[sort {$t[$a] cmp $t[$b]} 0 .. $#t]; }
+elsif($sort eq 'pl')   { my @t = map { (/^(?:[^<]*<>){5}([^<]*)/)[0]  } @list; @list = @list[sort {$t[$a] cmp $t[$b]} 0 .. $#t]; }
+elsif($sort eq 'date') { my @t = map { (/^(?:[^<]*<>){3}([^<]*)/)[0]  } @list; @list = @list[sort {$t[$b] <=> $t[$a]} 0 .. $#t]; }
+elsif($sort eq 'level'){ my @t = map { (/^(?:[^<]*<>){15}([^<]*)/)[0] } @list; @list = @list[sort {$t[$b] <=> $t[$a]} 0 .. $#t]; }
 
-sub sortName { $_[0] =~ s/^“.*”//; return $_[0]; }
+sub sortName { $_[0] =~ /^(?:[^<]*<>){4}(?:“\s*(.*?)”)?\s*(.*?)</; return $2 || $1; }
 
 ### リストを回す --------------------------------------------------
-my %count; my %pl_flag;
+my %count = ( PC => {}, PL => {} );
 my %grouplist;
 my $page = $::in{page} || 1;
 my $pagestart = $page * $set::pagemax - $set::pagemax + 1;
 my $pageend   = $page * $set::pagemax;
+if($::in{group} && $set::pagemax){
+  $count{PC}{$::in{group}} = scalar(@list);
+  $count{PL}{$::in{group}} = {};
+  if($set::playerlist){
+    $count{PL}{$::in{group}}{ (/^(?:[^<]*<>){5}([^<]*)/)[0] }++ foreach @list;
+  }
+  $pageend = $count{PC}{$::in{group}} if $pageend > $count{PC}{$::in{group}};
+  @list = @list[$pagestart-1 .. $pageend-1];
+}
 foreach (@list) {
   my (
     $id, undef, undef, $updatetime, $name, $player, $group, #0-6
@@ -172,17 +170,18 @@ foreach (@list) {
   $group = $set::group_default if (!$group || !$groups{$group});
   $group = 'all' if $::in{group} eq 'all';
   
-  #カウント
-  $count{PC}{$group}++;
-  $count{PL}{$group}++ if !$pl_flag{$group}{$player};
-  $pl_flag{$group}{$player} = 1;
+  unless($::in{group} && $set::pagemax){
+    #カウント
+    $count{PC}{$group}++;
+    $count{PL}{$group}{$player}++;
 
-  #表示域以外は弾く
-  if (
-    ( $index_mode && $count{PC}{$group} > $set::list_maxline && $set::list_maxline) || #TOPページ
-    (!$index_mode && $set::pagemax && ($count{PC}{$group} < $pagestart || $count{PC}{$group} > $pageend)) #それ以外
-  ){
-    next;
+    #表示域以外は弾く
+    if (
+      ( $index_mode && $count{PC}{$group} > $set::list_maxline && $set::list_maxline) || #TOPページ
+      (!$index_mode && $set::pagemax && ($count{PC}{$group} < $pagestart || $count{PC}{$group} > $pageend)) #それ以外
+    ){
+      next;
+    }
   }
 
   #名前
