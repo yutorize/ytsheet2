6360e655529a16edce3ff42c7cf9c598dbb8e960
TALE <42292222+yutorize@users.noreply.github.com>
2026-05-08 15:52:56 +0900

    ♻️ 変更: 一部の文字削除処理を`s///`から`y///`へ

--- _core/lib/ms/view-clan.pl
+++ _core/lib/ms/view-clan.pl
@@ -193,7 +193,7 @@ foreach (1 .. $pc{historyNum}){
   $h_num++ if $pc{'history'.$_.'Gm'};
   if ($set::log_dir && $pc{'history'.$_.'Date'} =~ s/([^0-9]*?_[0-9]+(?:#[0-9a-zA-Z]+?)?)$//){
     my $room = $1;
-    (my $date = $pc{'history'.$_.'Date'}) =~ s/[\-\/]//g;
+    (my $date = $pc{'history'.$_.'Date'}) =~ y#\-\/##d;
     $pc{'history'.$_.'Date'} = "<a href=\"$set::log_dir$date$room.html\">$pc{'history'.$_.'Date'}<\/a>";
   }
   if ($set::sessionlist && $pc{'history'.$_.'Title'} =~ s/^#([0-9]+)//){
