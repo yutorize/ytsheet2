b1d156f1170b3790cdd013a74a4ed9375e7cc582
TALE <42292222+yutorize@users.noreply.github.com>
2026-05-09 20:49:15 +0900

    :zap: 整理: 画像表示のメモリ消費改善

--- _core/lib/subroutine.pl
+++ _core/lib/subroutine.pl
@@ -163,30 +163,42 @@ sub changeFileByType {
 ### 画像リダイレクト --------------------------------------------------
 sub redirectToImage {
   my $id   = shift;
-  my $type = shift;
   my ($file,$type,$user) = findSheet($id);
   changeFileByType($type);
   my $datadir = $set::char_dir;
   my $ext;
 
-  if(!$file){ error("ファイルがありません。") }
+  if(!$file){ die "image not found" }
 
-  open(my $DATA, "./${datadir}/${file}/data.cgi") or die("file open error: $id:$file,$type // $!");
+  open(my $DATA, '<', "./${datadir}/${file}/data.cgi") or die("file open error: $id:$file,$type // $!");
   while(<$DATA>){
     if($_ =~ /^image<>(.*?)\n/){ $ext = $1; last }
   }
   close($DATA);
 
-  if(!$ext){ error("画像がありません。") }
+  if(!$ext){ die "invalid image extension" }
 
-  open(my $IMG, "./${datadir}/${file}/image.${ext}") or die("image open error: $id:$file,$type // $!");
+  my %mime = (
+    jpg  => 'image/jpeg',
+    jpeg => 'image/jpeg',
+    png  => 'image/png',
+    gif  => 'image/gif',
+    webp => 'image/webp',
+  );
+  my $mimeType = $mime{lc $ext} or die "bad ext";
+
+  my $path = "./${datadir}/${file}/image.${ext}";
+
+  open(my $IMG, '<', $path) or die("image open error: $id:$file,$type // $!");
+  my $size = -s $IMG;
   binmode $IMG;
   binmode STDOUT;
-  print "Content-type: image/".($ext eq 'jpg' ? 'jpeg' : $ext)."\n";
+  print "Content-type: $mimeType\n";
+  print "Content-Length: $size\n";
   print "Cache-Control: public, max-age=604800\n";
   print "Content-Disposition: inline; filename=\"ytsheet_$::in{id}.$ext\"\n";
   print "\n";
-  print while (<$IMG>);
+  while (read($IMG, my $buf, 65536)) { print $buf; }
   close($IMG);
   exit;
 }
