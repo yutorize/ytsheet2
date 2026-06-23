d47dffd4934086b009719663b00961da4ddf0911
TALE <42292222+yutorize@users.noreply.github.com>
2026-06-23 21:05:10 +0900

    :zap: 追加: 常にシングルカラムで表示するモード

--- _core/skin/_common/js/common.js
+++ _core/skin/_common/js/common.js
@@ -3,37 +3,49 @@ const rootClass = root.classList;
 // ナイトモード
 let nightMode = localStorage.getItem("nightMode");
 if(nightMode == 1) { rootClass.add('night'); }
-function nightModeChange() {
-  if(nightMode != 1) { rootClass.add('night');nightMode = 1; }
-  else { rootClass.remove('night'); nightMode = 0; }
+function toggleNightMode() {
+  if(nightMode != 1) { rootClass.add('night'); nightMode = 1; }
+  else               { rootClass.remove('night'); nightMode = 0; }
   localStorage.setItem("nightMode", nightMode);
 }
 // カラーカスタムON/OFF
 let colorlessMode = localStorage.getItem("colorlessMode");
 if(colorlessMode == 1) { rootClass.add('colorless'); }
-function changeColorlessMode(){
+function toggleColorlessMode(){
   if(colorlessMode != 1) { rootClass.add('colorless');    colorlessMode = 1; }
   else                   { rootClass.remove('colorless'); colorlessMode = 0; }
   localStorage.setItem("colorlessMode", colorlessMode);
 }
 window.addEventListener("DOMContentLoaded", () => {
   console.log('colorlessMode:'+colorlessMode);
-  const obj = document.querySelector('[onchange*=changeColorlessMode]') || '';
+  const obj = document.querySelector('[onchange*=toggleColorlessMode]') || '';
   if(obj && colorlessMode == 1){
     obj.checked = true;
   }
 })
 // ルビコピーON/OFF
 let rubyCopyMode = localStorage.getItem("rubyCopyMode") ?? 1;
-function changeRubyCopyMode(){
-  if(rubyCopyMode != 1) { rubyCopyMode = 1; }
-  else                  { rubyCopyMode = 0; }
+function toggleRubyCopyMode(){
+  rubyCopyMode = (rubyCopyMode != 1) ? 1 : 0;
   localStorage.setItem("rubyCopyMode", rubyCopyMode);
 }
 window.addEventListener("DOMContentLoaded", () => {
   console.log('rubyCopyMode:'+rubyCopyMode)
-  const obj = document.querySelector('[onchange*=changeRubyCopyMode]') || ''
+  const obj = document.querySelector('[onchange*=toggleRubyCopyMode]') || ''
   if(obj && rubyCopyMode == 1){
+    obj.checked = true;
+  }
+})
+// 1カラムモード
+let singleColumnMode = localStorage.getItem("singleColumnMode") ?? 0;
+function toggleSingleColumnMode(){
+  singleColumnMode = (singleColumnMode != 1) ? 1 : 0;
+  localStorage.setItem("singleColumnMode", singleColumnMode);
+}
+window.addEventListener("DOMContentLoaded", () => {
+  console.log('singleColumnMode:'+singleColumnMode)
+  const obj = document.querySelector('[onchange*=toggleSingleColumnMode]') || ''
+  if(obj && singleColumnMode == 1){
     obj.checked = true;
   }
 })
