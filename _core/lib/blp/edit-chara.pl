fac763097cb473964e9b6fcfd0d83dd492cb711f
TALE <42292222+yutorize@users.noreply.github.com>
2026-07-18 14:31:16 +0900

    ⚡ 追加: 現在編集中のシートに別のシートのデータで上書き編集する機能

--- _core/lib/blp/edit-chara.pl
+++ _core/lib/blp/edit-chara.pl
@@ -438,13 +438,13 @@ print <<"HTML";
     <details class="box" id="free-note" @{[$pc{freeNote}?'open':'']}>
       <summary class="in-toc">容姿・経歴・その他メモ</summary>
       <textarea name="freeNote">$pc{freeNote}</textarea>
-      @{[ $::in{log} ? '<button type="button" class="set-newest" onclick="setNewestSingleData(\'freeNote\')">最新のメモを適用する</button>' : '' ]}
+      @{[ ($::in{log} || $::in{overwrite}) ? '<button type="button" class="set-newest" onclick="setNewestSingleData(\'freeNote\')">最新のメモを適用する</button>' : '' ]}
     </details>
 
     <details class="box" id="free-history" @{[$pc{freeHistory}?'open':'']}>
       <summary class="in-toc">履歴（自由記入）</summary>
       <textarea name="freeHistory">$pc{freeHistory}</textarea>
-      @{[ $::in{log} ? '<button type="button" class="set-newest" onclick="setNewestSingleData(\'freeHistory\')">最新の履歴（自由記入）を適用する</button>' : '' ]}
+      @{[ ($::in{log} || $::in{overwrite}) ? '<button type="button" class="set-newest" onclick="setNewestSingleData(\'freeHistory\')">最新の履歴（自由記入）を適用する</button>' : '' ]}
     </details>
 
     <div class="box" id="history">
@@ -513,7 +513,7 @@ print <<"HTML";
       <ul class="annotate">
         <li>「力の向上」の選択を行うと、自動的に練度が+1されます。
       </ul>
-      @{[ $::in{log} ? '<button type="button" class="set-newest" onclick="setNewestHistoryData()">最新のセッション履歴を適用する</button>' : '' ]}
+      @{[ ($::in{log} || $::in{overwrite}) ? '<button type="button" class="set-newest" onclick="setNewestHistoryData()">最新のセッション履歴を適用する</button>' : '' ]}
     </div>
   </section>
 HTML
